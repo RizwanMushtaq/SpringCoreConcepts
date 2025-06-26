69b6548dfe74c43290ba5a5113040b94c3c6c77b
Rizwan <rizwanmushtaq15@gmail.com>
2025-06-26 17:44:39 +0200

    initial commit

---
+++ src/main/java/com/rizwan/core/SpringCoreConcepts/SpringCoreConceptsApplication.java
@@ -0,0 +1,11 @@
+package com.rizwan.core.SpringCoreConcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreConceptsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCoreConceptsApplication.class, args);
+  }
+}
---
+++ src/test/java/com/rizwan/core/SpringCoreConcepts/SpringCoreConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizwan.core.SpringCoreConcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
